51e84f179c5b1081ac9a79d5cd73440e36e664f4
Ro0ojena <90079740+Ro0ojena@users.noreply.github.com>
2021-12-08 00:45:26 +0300

    change background to white and color text+navigationDrawer

--- app/src/main/java/com/example/week6d1/MainActivity.kt
+++ app/src/main/java/com/example/week6d1/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.week6d1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 
 class MainActivity : AppCompatActivity() {
 
     private var textViewEmail:TextView?=null
+    private var drawerLayout:DrawerLayout?=null
+    private var toolBarView:Toolbar?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,11 +23,36 @@ class MainActivity : AppCompatActivity() {
 
 
         connectViews()
+        setSupportActionBar(toolBarView)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         textViewEmail?.text=email
+
+        setUpDrawer()
+
+    }
+    private fun setUpDrawer(){
+
+        val toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
+        drawerLayout?.addDrawerListener(toggle)
+        toggle.syncState()
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            android.R.id.home ->{
+                drawerLayout?.openDrawer(GravityCompat.START)
+                true
+            }
+            else->true
+        }
 
     }
     private fun connectViews(){
         textViewEmail=findViewById(R.id.tvEmail)
+        drawerLayout=findViewById(R.id.drawer)
+        toolBarView=findViewById(R.id.toolBar)
     }
+
 }
