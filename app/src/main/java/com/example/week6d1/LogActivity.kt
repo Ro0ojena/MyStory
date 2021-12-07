d643bc39f8ad245e79753ebb26f2b7b31f47b94d
Ro0ojena <90079740+Ro0ojena@users.noreply.github.com>
2021-12-07 20:15:48 +0300

    show username on Activity main add if for the empty fields

--- app/src/main/java/com/example/week6d1/LogActivity.kt
+++ app/src/main/java/com/example/week6d1/LogActivity.kt
@@ -1,5 +1,6 @@
 package com.example.week6d1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
@@ -17,6 +18,7 @@ class LogActivity : AppCompatActivity() {
 
         connectViews()
         login()
+//        checkFields()
     }
 
     private fun connectViews() {
@@ -40,7 +42,11 @@ class LogActivity : AppCompatActivity() {
             val user = User(userName, passWord)
             for (userArray in arr) {
                 if (userArray.email == user.email && userArray.passWord == user.passWord) {
-                    Toast.makeText(this, "Welcome ${user.email}", Toast.LENGTH_SHORT).show()
+//                    Toast.makeText(this, "Welcome ${user.email}", Toast.LENGTH_SHORT).show()
+    finish()
+    val i =Intent(this,MainActivity::class.java)
+                    i.putExtra("email",userArray.email)
+                    startActivity(i)
                     break
                 } else {
                     Toast.makeText(this, "email or password is wrong", Toast.LENGTH_SHORT).show()
@@ -48,18 +54,22 @@ class LogActivity : AppCompatActivity() {
                 }
             }
 
-            buttonLogin?.setOnClickListener {
-
-                if (editTextUserName?.text?.isEmpty() == true || editTextPassword?.text?.isEmpty() == true
-                    || checkBox?.isChecked == false
-                ) {
-                    Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
-                } else {
-                    Toast.makeText(this, "success", Toast.LENGTH_SHORT).show()
-                }
-            }
 
 
         }
     }
-}
+//    private fun checkFields(){
+//        buttonLogin?.setOnClickListener {
+//
+//            if (editTextUserName?.text?.isEmpty() == true ){
+//                editTextUserName?.setError("Enter your email")
+//            } else if(editTextPassword?.text?.isEmpty() == true) {
+//            editTextPassword?.setError("Enter your passWord")
+//
+//            }else{
+//                Toast.makeText(this, "success", Toast.LENGTH_SHORT).show()
+//            }
+//        }
+
+    }
+
